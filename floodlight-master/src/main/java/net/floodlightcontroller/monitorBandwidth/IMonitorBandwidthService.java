35a5c860c68882fb039038e331d8f7ee057144d1
lox <305235457@qq.com>
2022-01-12 17:25:08 +0800

    第一次提交

---
+++ floodlight-master/src/main/java/net/floodlightcontroller/monitorBandwidth/IMonitorBandwidthService.java
@@ -0,0 +1,12 @@
+package net.floodlightcontroller.monitorBandwidth;
+
+import java.util.Map;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.types.NodePortTuple;
+import net.floodlightcontroller.statistics.SwitchPortBandwidth;
+
+
+public interface IMonitorBandwidthService extends IFloodlightService {
+    //带宽使用情况
+    public Map<NodePortTuple,SwitchPortBandwidth> getBandwidthMap();
+}
